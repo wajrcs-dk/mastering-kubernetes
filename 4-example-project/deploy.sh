6bc09b410883cf5723571e05c12a8818ebceb8e4
Waqar Alamgir <alamgir@dzhw.de>
2024-02-01 16:24:28 +0100

    Main: Example project added.

---
+++ 4-example-project/deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Launch Minikube
+minikube start
+minikube addons enable ingress
+
+cd /vagrant_data/4-example-project/
+
+# Setup Mysql
+kubectl apply -f ./kubernetes/mysql/mysql-configmap.yaml
+kubectl apply -f ./kubernetes/mysql/mysql-services.yaml
+kubectl apply -f ./kubernetes/mysql/mysql-statefulset.yaml
+kubectl get pods -l app=mysql --watch
+
+# Building Project
+eval $(minikube docker-env)
+docker build . --target composer_base
+docker build . --target web_server
+docker compose build
+
+# Verify Images
+minikube image ls --format table
+
+# Setup Config Maps
+kubectl apply -f ./kubernetes/common/
+
+# Deploy FPM
+kubectl apply -f ./kubernetes/fpm/deployment.yml
+kubectl apply -f ./kubernetes/fpm/service.yml
+
+# Deploy Webserver
+kubectl apply -f ./kubernetes/webserver/deployment.yml
+kubectl apply -f ./kubernetes/webserver/service.yml
+
+# Deploy Redis
+kubectl apply -f ./kubernetes/redis/redis-configmap.yml
+kubectl apply -f ./kubernetes/redis/redis-statefulset.yml
+kubectl apply -f ./kubernetes/redis/redis-service.yml
+
+# Deploy PhpMyAdmin
+kubectl apply -f ./kubernetes/phpmyadmin/deployment.yml
+kubectl apply -f ./kubernetes/phpmyadmin/service.yml
+kubectl port-forward service/laravel-in-kubernetes-phpmyadmin 8080:80
+
+# Deploy Ingress
+kubectl apply -f ./kubernetes/ingress/ingress.yml
